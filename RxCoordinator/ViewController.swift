bac7ccaf5072d21af67ba8ebcc6984f4f7239a86
stasuwe <stasuwe@gmail.com>
2018-06-07 22:21:17 +0300

    Initial Commit

---
+++ RxCoordinator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxCoordinator
+//
+//  Created by Станислав Сарычев on 07.06.2018.
+//  Copyright © 2018 Станислав Сарычев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
